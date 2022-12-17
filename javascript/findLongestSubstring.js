a2a3f9d9cf5c703086ac89de6ebbd2573870a6fe
lyssoi <114637463+lyssoi@users.noreply.github.com>
2022-12-18 00:21:46 +0900

    Update findLongestSubstring.js
    솔루션

--- javascript/findLongestSubstring.js
+++ javascript/findLongestSubstring.js
@@ -1,25 +1,22 @@
-function findLongestSubstring(arr){
+function findLongestSubstring(str){
     // add whatever parameters you deem necessary - good luck!
-    /*집합을 만들어서 하나씩 추가하면서 indexof로 요소 찾기. 
-    같은 요소가 있다면 그 왼쪽에 있는 요소는 지워버릴것
-    이때 길이는 저장*/
-    let findArr = [];
-    let strLen = 0;
-    let pointer =0;
-    let longStrLen=0;
-    let indexPointer = 0;
-    
-    while(pointer<arr.length){
-        if(findArr.indexof('arr[pointer]')!==-1){
-            findArr.push(arr[pointer])
-            strLen = findArr.length;
-            pointer++;
+    /*start포인터를 사용하여 substring의 시작점을 조정
+    dictionary를 이용하여 문자의 위치를 저장함
+    (중복된 문자를 만났을때, 그 다음부터 시작하기 위함)
+    문자열의 길이를 재고, 가장 긴 길이만 저장함
+    */
+    let longest = 0;
+    let start = 0;
+    let strIndex = {};
+
+    for(let i=0;i<str.length;i++){
+        let char = str[i];
+        if(strIndex[char]){
+            start = Math.max(start,strIndex[char]+1)
+            //start위치와 strIndex[char]의 위치를 비교하여, strIndex[char]이 start보다 먼저 있는 경우 무시하기 위함
         }
-        else{
-            indexPointer = indexof(findArr[pointer]);
-            findArr = findArr.slice(indexPointer);
-        longStrLen = Math.max(strLen,findArr.length);
-        pointer++;}
+        longest = Math.max(longest,start-i+1);
+        strIndex[char]=i;
     }
-    return longStrLen;
+    return longest;
   }
