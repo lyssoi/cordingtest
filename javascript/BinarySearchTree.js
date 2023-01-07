df550cce020f33becbdbbf9c1176ce07b1a1ba4c
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-01-07 17:29:12 +0900

    Update BinarySearchTree.js

--- javascript/BinarySearchTree.js
+++ javascript/BinarySearchTree.js
@@ -10,16 +10,16 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  insert(val) {
-    let newNode = new Node(val);
+  insert(value) {
+    let newNode = new Node(value);
     if (!this.root) {
       this.root = newNode;
       return this;
     }
     let current = this.root;
     while (true) {
-      if (val === current.value) return undefined;
-      if (val < current.value) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
         if (current.left === null) {
           current.left = newNode;
           return this;
@@ -64,14 +64,63 @@ class BinarySearchTree {
     }
     return false;
   }
+  BFS() {
+    let node = this.root,
+      queue = [],
+      data = [];
+    queue.push(node);
+    // queue = [root];
+    // data = [];
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  } //길~게 늘어진 트리구조에 적합(넓은 트리구조엔 비적합함!!! 공간복잡도가 높아서!!!)
+  DFSPreOrder() {
+    let data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  } // 장점 : 트리구조를 다시 만들수 있음!(root가 처음이고 계속 자식에 추가하면 트리구조가 나옴)
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  } //장점을...모르게써여...
+  DFSInOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  } //장점 : 오름차순으로 정렬됨.
 }
 
 let tree = new BinarySearchTree();
 
-tree.insert(15);
-tree.insert(5);
-tree.insert(3);
-tree.insert(7);
+tree.insert(15); //     15
+tree.insert(5); //  5      //20
+tree.insert(3); //3  7       //25
+tree.insert(7); //     10
 tree.insert(10);
 tree.insert(20);
 tree.insert(25);
+
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
+console.log(tree.DFSInOrder());
