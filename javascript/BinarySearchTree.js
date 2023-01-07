7e8e5558dc14afb6f737859aa4ae386174432747
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-01-07 16:16:09 +0900

    Create BinarySearchTree.js

---
+++ javascript/BinarySearchTree.js
@@ -0,0 +1,77 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.value) return undefined;
+      if (val < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+  find(value) {
+    if (!this.root) return undefined;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else if (value === current.value) {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (!this.root) return false;
+    let current = this.root;
+    while (current) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+let tree = new BinarySearchTree();
+
+tree.insert(15);
+tree.insert(5);
+tree.insert(3);
+tree.insert(7);
+tree.insert(10);
+tree.insert(20);
+tree.insert(25);
